ac1c78763e196cf4237849b8ff93bf867ad35eef
David Roach <deroach88@gmail.com>
2015-12-10 15:24:57 -0500

    Updated Content Area

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -1,26 +1,28 @@
 <?php
 /**
- * Template Name: DRD &raquo; About
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
+* Template Name: DRD &raquo; About
+*
+* @package WordPress
+* @subpackage Twenty_Fourteen
+* @since Twenty Fourteen 1.0
+*/
 
 get_header(); ?>
 
-		<?php get_template_part( 'includes/include-sidebar' ); ?>
+<?php
+$content = types_render_field( 'content', array( 'raw' => true ) );
+?>
 
-		<section id="case-study">
-			<div class="overview">
-				<div class="container">
-					<h1><?php the_title(); ?></h1>
-					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-						<?php the_content(); ?>
-					<?php endwhile; endif; ?>
-				</div>
-			</div>
-			<?php get_template_part( 'includes/include-screens' ); ?>
-		</section>
+<?php get_template_part( 'includes/include-sidebar' ); ?>
+
+<section id="about">
+<div class="overview">
+<div class="container">
+<h1><?php the_title(); ?></h1>
+<?php echo $content; ?>
+</div>
+</div>
+<?php get_template_part( 'includes/include-screens' ); ?>
+</section>
 
 <?php get_footer(); ?>
